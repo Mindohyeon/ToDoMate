56f96d614095b7877efc20673f99519a60b28542
Mindohyeon <min50875@gmail.com>
2022-03-06 00:18:16 +0900

    화면에 오늘의 날짜에 대한 일주일이 안 뜸

--- ViewModel/CustomCalendarViewModel.swift
+++ ViewModel/CustomCalendarViewModel.swift
@@ -17,12 +17,12 @@ class CustomCalendarViewModel : ObservableObject {
         
         let calendar = Calendar.current
         
-        guard let currentWeek = calendar.date(byAdding: .weekday, value: self.currnetWeek, to: Date())
+        guard let currentMonth = calendar.date(byAdding: .weekOfMonth, value: self.currnetWeek, to: Date())
         else {
             return Date()
         }
-        
-        return currentWeek
+        print(currnetWeek)
+        return currentMonth
         
     }
     
@@ -31,6 +31,44 @@ class CustomCalendarViewModel : ObservableObject {
           
           return calendar.isDate(date1, inSameDayAs: date2)
       }
+    
+    
+    func extraDate() -> [String] {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY MMMM"
+        
+        let date = formatter.string(from: currentDate)
+        
+        print(date)
+
+        return date.components(separatedBy: " ")
+    }
+    
+    
+    func extractDate() -> [DateValue] {
+        
+        let calendar = Calendar.current
+        
+        //Getting current Month Date
+        let currentMonth = getCurrentWeek()
+        
+        var days = currentMonth.getAllDates().compactMap { date -> DateValue in
+            
+            let day = calendar.component(.day, from: date)
+            return DateValue(day: day, date: date)
+        }
+        
+        // Adding offset days to get exact week day
+        let firstWeekday = calendar.component(.weekday, from: days.first?.date ?? Date())
+    
+        
+        for _ in 0..<firstWeekday - 1{
+            days.insert(DateValue(day: -1, date: Date()), at: 0)
+            print(currentDate)
+        }
+        
+        return days
+    }
     
     
 }
