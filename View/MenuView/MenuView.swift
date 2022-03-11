43f027fe6420a02a12758670b65d993403bf38b5
Mindohyeon <min50875@gmail.com>
2022-03-11 11:32:18 +0900

    [Create] MenuView

---
+++ View/MenuView/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  ToDoMate
+//
+//  Created by 민도현 on 2022/03/11.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        CustomAddViewCell()
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
