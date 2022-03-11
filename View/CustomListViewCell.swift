9bbf4232f528c69aadff84e7407d716e7e5737bd
Mindohyeon <min50875@gmail.com>
2022-03-11 09:42:49 +0900

    [Create] CustomListViewCell

---
+++ View/CustomListViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  CustomListViewCell.swift
+//  ToDoMate
+//
+//  Created by 민도현 on 2022/03/11.
+//
+
+import SwiftUI
+
+struct CustomListViewCell: View {
+    
+    var item : TodoListItem?
+    
+    init(_ item : TodoListItem) {
+        self.item = item
+    }
+    
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Text("check")
+            }
+            
+            Text("item : \(item!.name)")
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "ellipsis")
+                    .font(.title2)
+                    .foregroundColor(.gray)
+                
+            }
+        }
+        .padding()
+    }
+}
+
+
+
+
+
+struct CustomListViewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomListViewCell()
+    }
+}
