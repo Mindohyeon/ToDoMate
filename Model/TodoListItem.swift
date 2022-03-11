2e5b0e948d8a361fd0776dfc1ec395682e20574c
Mindohyeon <min50875@gmail.com>
2022-03-11 10:28:19 +0900

    [Cretae] TodoListItem

---
+++ Model/TodoListItem.swift
@@ -0,0 +1,16 @@
+//
+//  TodoListItem.swift
+//  ToDoMate
+//
+//  Created by 민도현 on 2022/03/11.
+//
+
+import SwiftUI
+
+struct TodoListItem : Identifiable {
+    
+    var id = UUID().uuidString
+    var name : String
+    
+    
+}
