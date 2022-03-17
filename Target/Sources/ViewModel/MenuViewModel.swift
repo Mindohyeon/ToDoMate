990dd609231463be94e1b6492433b47f3aae954c
Mindohyeon <min50875@gmail.com>
2022-03-17 17:51:12 +0900

    [Error] MenuViewModel - TodoListItem 에 Field 값 넣기

--- Target/Sources/ViewModel/MenuViewModel.swift
+++ Target/Sources/ViewModel/MenuViewModel.swift
@@ -12,5 +12,15 @@ class MenuViewModel : ObservableObject {
     
     @Published var inputText : String = ""
     
+    var item : [TodoListItem] = []
+    
+    func setList(item: String) {
+        for i in 0...self.item.count {
+        self.item[i].name = item
+            print(self.item[i])
+        }
+    }
+
+    
     
 }
