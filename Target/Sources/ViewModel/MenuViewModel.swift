9f72f126fb671afedf9c6429a2f2106c610df316
Mindohyeon <min50875@gmail.com>
2022-04-11 09:32:11 +0900

    for문 개수 변경

--- Target/Sources/ViewModel/MenuViewModel.swift
+++ Target/Sources/ViewModel/MenuViewModel.swift
@@ -12,12 +12,13 @@ class MenuViewModel : ObservableObject {
     
     @Published var inputText : String = ""
     
-    var item : [TodoListItem] = []
+    @Published var item : [TodoListItem] = []
     
     func setList(item: String) {
-        for i in 0...self.item.count {
-        self.item[i].name = item
-            print(self.item[i])
+        for i in 0...2 {
+//        self.item[i].name = item
+//            print(self.item[i])
+            print("MenuViewModel = \(inputText)")
         }
     }
 
